b8414418b196adea57de252cf9c5088d75713605
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-31 22:58:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
-def using_push(countries_in_western_africa, string)
-  string.push 
+def using_push(countries_in_western_africa, next_country)
+  next_country.push 
 end
 
 def using_unshift(array,string)
