e7d4699f1ff8a27f3d956310a9b3d81f6557cf2f
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-31 23:06:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,7 @@ def shift_with_args(ice_cream_brands)
   array.shift(2)
 end
 
-def using_concat(array1,array2)
+def using_concat(my_favorite_things, more_favs)
   array.concat(array1, array2)
 end
 
