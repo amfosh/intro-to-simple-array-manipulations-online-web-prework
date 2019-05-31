3cfeb1e4d7b05fd3a0496b5e7e15dd45008c3b35
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-31 23:20:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,7 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def unsing_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
   list_of_esoteric_programming_languages.insert(another_esoteric_language)
 end
 
